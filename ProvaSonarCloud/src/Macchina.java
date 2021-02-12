5334342199d2ec4defa200dd2904486b34b35b30
luca-capotombolo <66474292+luca-capotombolo@users.noreply.github.com>
2021-02-12 17:39:09 +0100

    versione due, prova bug classe Macchina.java

---
+++ ProvaSonarCloud/src/Macchina.java
@@ -0,0 +1,14 @@
+
+public class Macchina {
+	
+	private char[] targa = new char[8];
+	private char[] proprietario = new Proprietario("Luca");
+	private short eta = 0;
+	
+	public Macchina() {
+		
+	}
+	
+	
+	
+}
